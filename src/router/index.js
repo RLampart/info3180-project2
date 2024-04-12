5e9ee5c7b6de961ac131a329621c537ca42d8c58
Rahiem Lampart <113929401+RLampart@users.noreply.github.com>
2024-04-12 16:20:47 -0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,52 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/register',
+      name: 'register',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/AboutView.vue')//Change later
+    },
+    {
+      path: '/login',
+      name: 'login',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/LoginView.vue')//Change later
+    },
+    {
+      path: '/logout',
+      name: 'logout',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Logout.vue')//Change later
+    },
+    {
+      path: '/explore',
+      name: 'explore',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ExploreView.vue')//Change later
+    },
+    {
+      path: '/users/{user_id}',
+      name: 'user',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Logout.vue')//Change later
+    },
+    {
+      path: '/posts/new',
+      name: 'new_posts',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Posts.vue')//Change later
     }
   ]
 })
