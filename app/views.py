0ae32df6f81f330b2b26911726d55865a4a187c7
Amoy Perry <amoyperry123@gmail.com>
2024-04-13 23:21:15 -0500

    Models and Migration

--- app/views.py
+++ app/views.py
@@ -5,10 +5,17 @@ Werkzeug Documentation:  https://werkzeug.palletsprojects.com/
 This file creates your application.
 """
 
+from flask import Flask, make_response, request, jsonify
 from app import app
-from flask import render_template, request, jsonify, send_file
+from flask import render_template, request, redirect, url_for, flash, send_from_directory
+from app.forms import *
+from .models import *
+from werkzeug.utils import secure_filename
+from werkzeug.security import generate_password_hash
+from app import app, db
 import os
-
+from flask_wtf.csrf import generate_csrf
+from datetime import datetime
 
 ###
 # Routing for your application.
@@ -18,7 +25,55 @@ import os
 def index():
     return jsonify(message="This is the beginning of our API")
 
-
+@app.route('/api/v1/register',methods =["POST"])
+def register():
+    form = Register()
+    data = {}
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        email = form.email.data
+        location = form.location.data
+        bio = form.bio.data
+        profile_photo = form.photo.data 
+        date_joined = datetime.now()
+        photo_name  =  secure_filename(profile_photo.filename)
+        profile_photo.save(os.path.join(
+            app.config['UPLOAD_FOLDER'], photo_name
+            ))
+        user = User(
+        username = username,
+        password = generate_password_hash(password),
+        firstname = first_name,
+        lastname = last_name,
+        email = email,
+        location = location,
+        biography = bio,
+        profile_photo = photo_name
+        )
+        db.session.add(user)
+        db.session.commit()
+        data = {
+            "message": "User successfully registered",
+            "username": username,
+            "password": password,
+            "firstname": first_name,
+            "lastname": last_name,
+            "email": email,
+            "location": location,
+            "biography": bio,
+            "profile_photo": photo_name,
+            "joined_on": date_joined
+        }
+    else:
+        data = {
+            "errors":[
+                {error.split(" - ")[0]:error.split(" - ")[1]} for error in form_errors(form)
+                    ]
+        }
+    return make_response(data,200)
 ###
 # The functions below should be applicable to all Flask apps.
 ###
