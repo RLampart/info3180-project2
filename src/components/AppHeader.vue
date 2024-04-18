0e8bfc0693002e0de4eb9db06e2efd2624c1ea20
qxeenolight <banana13.tp@gmail.com>
2024-04-18 11:18:13 -0500

    Added View for Homepage and Added Other Views

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,7 +2,7 @@
   <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
       <div class="container-fluid">
-        <a class="navbar-brand" href="/">VueJS with Flask</a>
+        <a class="navbar-brand label" href="/"><i class="bi bi-camera icon"></i> Photogram</a>
         <button
           class="navbar-toggler"
           type="button"
@@ -15,12 +15,18 @@
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto">
+          <ul class="navbar-nav ms-auto">
             <li class="nav-item">
               <RouterLink to="/" class="nav-link active">Home</RouterLink>
             </li>
             <li class="nav-item">
               <RouterLink class="nav-link active" to="/explore">Explore</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link active" to="/">My Profile</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link active" to="/logout">Logout</RouterLink>
             </li>
           </ul>
         </div>
@@ -34,5 +40,13 @@ import { RouterLink } from "vue-router";
 </script>
 
 <style>
-/* Add any component specific styles here */
+.label {
+  font-family: 'Brush Script MT', cursive;
+  font-size: 20px
+}
+
+.icon {
+  color: black;
+  padding-right: 5px;
+}
 </style>
